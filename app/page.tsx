a1667cd39caf76d116d272f2ae812e8e0e1a91c7
Kishor <kishor81160@gmail.com>
2025-12-21 23:28:49 +0530

    updated via env

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
-import Hero from '@/components/sections/Hero';
-import About from '@/components/sections/About';
-import Services from '@/components/sections/Services';
-import Portfolio from '@/components/sections/Portfolio';
-import Testimonials from '@/components/sections/Testimonials';
-import Contact from '@/components/sections/Contact';
-import Footer from '@/components/sections/Footer';
-import Header from '@/components/layout/Header';
-import CommingSoon from '@/components/comingsoon';
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
+import Services from "@/components/sections/Services";
+import Portfolio from "@/components/sections/Portfolio";
+import Testimonials from "@/components/sections/Testimonials";
+import Contact from "@/components/sections/Contact";
+import Footer from "@/components/sections/Footer";
+import Header from "@/components/layout/Header";
+import CommingSoon from "@/components/comingsoon";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
-      {/* <Header /> */}
-      <main>
-        {/* <Hero />
-        <About />
-        <Services />
-        <Portfolio />
-        <Testimonials />
-        <Contact /> */}
-        <CommingSoon/>
-      </main>
-      {/* <Footer /> */}
+      {process.env.IS_MENTAINCE == "true" ? (
+        <main>
+          <CommingSoon />
+        </main>
+      ) : (
+        <>
+          {" "}
+          <Header />
+          <main>
+            <Hero />
+            <About />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
